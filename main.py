8f65561e850434ac08b5dfcad19dde649cb6870c
Arnab Maity <arnabbibhuti01@gmail.com>
2022-06-22 07:39:23 +0000

    Schema Validations using Pydantic

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel # Pydantic is used to make schemas
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    #it's doing all the kind of validations we need (Pydantic)
+    title: str
+    content: str
+    published: bool = True  #default value
+    rating: Optional[int] = None #Optional Value
 
 #@app.get -> Method , ("/") -> path
 
@@ -13,10 +22,24 @@ async def root():
 def get_posts():
     return {"data" : "This is your posts"}
 
+# Without using schemas
+# @app.post("/createposts")
+# def create_posts(payLoad: dict = Body(...)):   #Payload 
+    # print(payLoad)
+    # return {
+    #     "message": "successfully created post",
+    #     "new_post": f"title {payLoad['title']} content: {payload['content']}"
+    # }
+
+#By using Schemas
 @app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):   #Payload 
-    print(payLoad)
-    return {
-        "message": "successfully created post",
-        "new_post": f"title {payLoad['title']} content: {payload['content']}"
-    }
+def create_posts(new_post: Post):  
+    print(new_post)
+    print(new_post.title)
+    print(new_post.dict())
+    return {"data" : "new post"}
+
+
+#tile strr, content str
+
+# We need a schema for proper validation 
