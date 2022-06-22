52be46fc0e01ea2b9e5cf49330b5404e507cfcdf
Arnab Maity <arnabbibhuti01@gmail.com>
2022-06-22 06:52:32 +0000

    Simple HTTP Requests in Fast APIs

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
+#@app.get -> Method , ("/") -> path
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-    
+    return {"message": "Welcome to fast API"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data" : "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):   #Payload 
+    print(payLoad)
+    return {
+        "message": "successfully created post",
+        "new_post": f"title {payLoad['title']} content: {payload['content']}"
+    }
